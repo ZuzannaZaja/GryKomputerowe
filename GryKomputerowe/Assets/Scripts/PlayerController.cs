60de582886010e37e665572f4ac584fabb12f9c3
igabiernat <igabiernat@gmail.com>
2020-05-17 15:25:34 +0100

    Add project in unity, basic functionalities - moving, rotating view

---
+++ GryKomputerowe/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private CharacterController characterController;
+    public float speed = 5f;
+    private Vector3 moveDirection = Vector3.zero;
+    // Start is called before the first frame update
+    void Start()
+    {
+        characterController = GetComponent<CharacterController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float x = Input.GetAxis("Horizontal");
+        float z = Input.GetAxis("Vertical");
+
+        Vector3 move = transform.right * x + transform.forward * z;
+        characterController.Move(move * speed * Time.deltaTime);
+    }
+}
