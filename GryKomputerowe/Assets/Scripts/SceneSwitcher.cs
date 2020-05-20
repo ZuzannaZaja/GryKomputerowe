574da584510646602f3ae8471ace0d7e830c5020
Zuzanna Zajaczkowska <zuzannazaja@gmail.com>
2020-05-20 19:03:41 +0200

    Start menu added.

---
+++ GryKomputerowe/Assets/Scripts/SceneSwitcher.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneSwitcher : MonoBehaviour
+{
+    public void StartGame()
+    {
+        SceneManager.LoadScene("Main");
+    }
+    public void QuitGame()
+    {
+        Application.Quit();
+
+    }
+    public void BackMenu()
+    {
+        SceneManager.LoadScene("Menu");
+
+    }
+}
+
